e8cfb457d8a0ac4694829ee8eed40138d184fe0c
Ronit Chatterjee <theronitchatterjee@gmail.com>
2018-10-29 16:44:21 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = [
+    { name: 'Ronit', regno: '11505399'},
+    { name: 'Nitish', regno: '11505711'},
+
+  ];
+  dbo.collection("Students").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
